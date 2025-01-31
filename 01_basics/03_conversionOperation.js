2457c91db2cfb2cfc9581bb4681a213a274559e2
Jeel Chheta <jeelchheta123@gmail.com>
2025-01-31 14:04:42 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+// console.log(typeof score); //string
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //Number
+// console.log(valueInNumber); //NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn = "Jeel"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
+
+// 1 => true; 0 => false
+// "" => false
+// "Jeel" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
