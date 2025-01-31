af9eede0991f6990e66892b560cd1bfea72e04e1
Jeel Chheta <jeelchheta123@gmail.com>
2025-01-31 16:24:55 +0000

    add string file

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "Jeel"
+const repoCount = 10
+
+// console.log(name + repoCount + " Value");
+
+// console.log(` Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Jeel-fc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-6,4)
+// console.log(anotherString);
+
+const newString1 = "      Jeel     "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url = "https://jeel.com/jeel%20chheta"
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('jeel'))
+
+// console.log(gameName.split('-'));
+
+
+
