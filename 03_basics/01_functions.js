e58798c61d9511ed626a4076a300274badce826a
Jeel Chheta <jeelchheta123@gmail.com>
2025-02-01 12:03:26 +0000

    Learn about Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,28 @@
+function sayMyName(){
+    console.log("J");
+    console.log("e");
+    console.log("e");
+    console.log("l");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
+// }
+function addTwoNumbers(num1, num2){
+    // let result = num1 + num2
+    // return result
+    return num1 + num2
+}
+
+const result = addTwoNumbers(3, 4)
+// console.log("Result: ",result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Jeel"))
+// console.log(loginUserMessage()) //undefined just logged in
+
