2e1a9f094c8bf98a1796e46286f9329c67a6c2ef
Jeel Chheta <jeelchheta123@gmail.com>
2025-02-01 13:48:48 +0000

    learn more about scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,11 +2,53 @@
 let a = 300
 
 if(true){
-    let a = 10
-    const b = 20
-    console.log("INNER: ", a);
+    // let a = 10
+    // const b = 20
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "Jeel"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if (true) {
+    const username = "Jeel"
+    if(username === "Jeel"){
+        const website = " youtube"
+        // console.log(username + website);
+    } 
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// +++++++++++++++ interesting ++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+
+const addtwo = function(num){
+    return num + 2
+}
+
+addtwo(5)
