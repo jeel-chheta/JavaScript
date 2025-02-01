8d61d35abea084b5195045600a0daaf07da0389a
Jeel Chheta <jeelchheta123@gmail.com>
2025-02-01 12:52:11 +0000

    Add more content

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,3 +26,31 @@ function loginUserMessage(username){
 // console.log(loginUserMessage("Jeel"))
 // console.log(loginUserMessage()) //undefined just logged in
 
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user= {
+    username:"Jeel",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+// handleObject(user)
+handleObject({
+    username:"Jee",
+    price:300
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
